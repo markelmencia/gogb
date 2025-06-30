392993b870b4b9cc513fe922cb55c911f3a2881b
Markel Mencía <markel.mnc@gmail.com>
2025-06-30 23:54:18 +0200

    Create instructions package

---
+++ cpu/instructions/sm89.go
@@ -0,0 +1,15 @@
+package instructions
+
+import (
+	"github.com/markelmencia/gogb/cpu"
+	"github.com/markelmencia/gogb/emulator"
+)
+
+// LD r, r': Load register (register) (8-Bit)
+//
+// Loads the value of r' into r
+func LDrr(dst, src cpu.Halve, emu emulator.Emulation) {
+	v := emu.CPU.Get(src)
+	emu.CPU.Set(dst, v)
+	emu.CPU.PC++
+}
