3c75fc2d5a28ecd67a33d25877831096deaa187f
Markel Mencía <markel.mnc@gmail.com>
2025-06-30 23:53:24 +0200

    Change getter/setter structure

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -8,6 +8,126 @@ const (
 
 type Halve byte
 
+const (
+	A Halve = iota
+	F
+	B
+	C
+	D
+	E
+	H
+	L
+)
+
+// Interface that stores both
+// the getter and the setter
+// of a halve.
+type HalveAccessor struct {
+	Get func(*CPU) byte
+	Set func(*CPU, byte)
+}
+
+// Getter note: In order to get the isolated value of a high 8-bit
+// register an AND operation must be performed to unset the lower
+// end of the 16-bit register via a mask (see HIGH_MASK and LOW_MASK).
+// Then, as the value is on the higher end, it must be shifted to the
+// lower end with a shift right operation in order to get the actual value.
+
+// Setter Note: To set an 8-bit register, it is important to unset the register
+// first, so the previous value does not overlap with the value to be set (v).
+// That is why before the set a masking AND operation is performed in the 16-bit register,
+// to only keep the halve of the register we do not want to change.
+
+// Map that contains a getter/setter pair
+// for each Halve register
+var halveToAccessor = map[Halve]HalveAccessor{
+	A: {
+		Get: func(c *CPU) byte {
+			return byte(c.AF >> 8)
+		},
+		Set: func(c *CPU, v byte) {
+			c.AF = (c.AF & LOW_MASK) | (uint16(v) << 8)
+		},
+	},
+	F: {
+		Get: func(c *CPU) byte {
+			return byte(c.AF)
+		},
+		Set: func(c *CPU, v byte) {
+			v16 := uint16(v)
+			c.AF = c.AF & HIGH_MASK
+			c.AF |= v16
+		},
+	},
+	B: {
+		Get: func(c *CPU) byte {
+			return byte(c.BC >> 8)
+		},
+		Set: func(c *CPU, v byte) {
+			c.BC = (c.BC & LOW_MASK) | (uint16(v) << 8)
+		},
+	},
+	C: {
+		Get: func(c *CPU) byte {
+			return byte(c.BC)
+		},
+		Set: func(c *CPU, v byte) {
+			v16 := uint16(v)
+			c.BC = c.BC & HIGH_MASK
+			c.BC |= v16
+		},
+	},
+	D: {
+		Get: func(c *CPU) byte {
+			return byte(c.DE >> 8)
+		},
+		Set: func(c *CPU, v byte) {
+			c.DE = (c.DE & LOW_MASK) | (uint16(v) << 8)
+		},
+	},
+	E: {
+		Get: func(c *CPU) byte {
+			return byte(c.DE)
+		},
+		Set: func(c *CPU, v byte) {
+			v16 := uint16(v)
+			c.DE = c.DE & HIGH_MASK
+			c.DE |= v16
+		},
+	},
+	H: {
+		Get: func(c *CPU) byte {
+			return byte(c.HL >> 8)
+		},
+		Set: func(c *CPU, v byte) {
+			c.HL = (c.HL & LOW_MASK) | (uint16(v) << 8)
+		},
+	},
+	L: {
+		Get: func(c *CPU) byte {
+			return byte(c.HL)
+		},
+		Set: func(c *CPU, v byte) {
+			v16 := uint16(v)
+			c.HL = c.HL & HIGH_MASK
+			c.HL |= v16
+		},
+	},
+}
+
+// Gets the appropiate register getter
+// and returns the value of the register
+func (c *CPU) Get(h Halve) byte {
+	return halveToAccessor[h].Get(c)
+}
+
+// Gets the appropiate register setter
+// and sets v into the register
+func (c *CPU) Set(h Halve, v byte) {
+	halveToAccessor[h].Set(c, v)
+}
+
+// Represents a GB CPU.
 type CPU struct {
 	// Registers
 	// Combined registers (two 8-bit registers combined)
@@ -21,106 +141,4 @@ type CPU struct {
 	IE uint16 // Interrupt Enable
 	SP uint16 // Stack Pointer
 	PC uint16 // Program Counter
-}
-
-// Getters and Setters
-
-// Getter note: In order to get the isolated value of a high 8-bit
-// register an AND operation must be performed to unset the lower
-// end of the 16-bit register via a mask (see HIGH_MASK and LOW_MASK).
-
-// Then, as the value is on the higher end, it must be shifted to the
-// lower end with a shift right operation in order to get the actual value.
-
-// Setter Note: To set an 8-bit register, it is important to unset the register
-// first, so the previous value does not overlap with the value to be set (v).
-// That is why before the set a masking AND operation is performed in the 16-bit register,
-// to only keep the halve of the register we do not want to change.
-
-// High 8-bit register getters and setters
-
-func (c CPU) GetA() byte {
-	masked := c.AF & HIGH_MASK
-	return byte(masked >> 8)
-}
-
-func (c CPU) GetB() byte {
-	masked := c.BC & HIGH_MASK
-	return byte(masked >> 8)
-}
-
-func (c CPU) GetD() byte {
-	masked := c.DE & HIGH_MASK
-	return byte(masked >> 8)
-}
-
-func (c CPU) GetH() byte {
-	masked := c.HL & HIGH_MASK
-	return byte(masked >> 8)
-}
-
-func (c *CPU) SetA(v byte) {
-	v16 := uint16(v)
-	c.AF = c.AF & LOW_MASK
-	c.AF += v16 << 8
-}
-
-func (c *CPU) SetB(v byte) {
-	v16 := uint16(v)
-	c.BC = c.BC & LOW_MASK
-	c.BC += v16 << 8
-}
-
-func (c *CPU) SetD(v byte) {
-	v16 := uint16(v)
-	c.DE = c.DE & LOW_MASK
-	c.DE += v16 << 8
-}
-
-func (c *CPU) SetH(v byte) {
-	v16 := uint16(v)
-	c.HL = c.HL & LOW_MASK
-	c.HL += v16 << 8
-}
-
-// Low 8-bit register getters and setters
-
-func (c CPU) GetF() byte {
-	return byte(c.AF & LOW_MASK)
-}
-
-func (c CPU) GetC() byte {
-	return byte(c.BC & LOW_MASK)
-}
-
-func (c CPU) GetE() byte {
-	return byte(c.DE & LOW_MASK)
-}
-
-func (c CPU) GetL() byte {
-	return byte(c.HL & LOW_MASK)
-}
-
-func (c *CPU) SetF(v byte) {
-	v16 := uint16(v)
-	c.AF = c.AF & HIGH_MASK
-	c.AF += v16
-}
-
-func (c *CPU) SetC(v byte) {
-	v16 := uint16(v)
-	c.BC = c.BC & HIGH_MASK
-	c.BC += v16
-}
-
-func (c *CPU) SetE(v byte) {
-	v16 := uint16(v)
-	c.DE = c.DE & HIGH_MASK
-	c.DE += v16
-}
-
-func (c *CPU) SetL(v byte) {
-	v16 := uint16(v)
-	c.HL = c.HL & HIGH_MASK
-	c.HL += v16
 }
