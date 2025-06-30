86a9c636015f77e21b68b30b1956965f1c46469e
Markel Mencía <markel.mnc@gmail.com>
2025-06-30 23:53:53 +0200

    Create Emulation struct

---
+++ emulator/emulator.go
@@ -0,0 +1,13 @@
+package emulator
+
+import (
+	"github.com/markelmencia/gogb/cpu"
+	"github.com/markelmencia/gogb/ram"
+)
+
+// Represents an instance of an emulation
+type Emulation struct {
+	CPU cpu.CPU
+	RAM ram.RAM
+	ROM []byte
+}
