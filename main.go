d47f81bf6b370905af234d50cfd9aa6a680a23eb
Markel Mencía <markel.mnc@gmail.com>
2025-06-30 13:51:45 +0200

    Create main method

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/markelmencia/gogb/cartridge"
+)
+
+func main() {
+	var header bool
+	flag.BoolVar(&header, "header", true, "Prints information about the specified ROM file")
+	flag.Parse()
+
+	l := log.New(os.Stderr, "gogb: ", 0)
+
+	if len(os.Args) < 2 {
+		l.Fatal("not enough arguments: please specify the ROM path")
+	}
+
+	if header {
+		if len(os.Args) < 3 {
+			l.Fatal("not enough arguments: please specify the ROM path to read the cartridge header")
+		}
+		romPath := os.Args[2]
+		cart, err := cartridge.GetCartridgeData(romPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cartridge.PrintHeaderData(cart)
+	}
+}
