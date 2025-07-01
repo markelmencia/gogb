65362b804193824f981e8a768a18e9fe107f64da
Markel Mencía <markel.mnc@gmail.com>
2025-07-01 11:46:55 +0200

    Create RAM setter

--- ram/ram.go
+++ ram/ram.go
@@ -4,6 +4,11 @@ type RAM [65536]byte
 
 // Returns the byte stored in the
 // specified memory address.
-func (r RAM) GetByte(a uint16) byte {
+func (r *RAM) GetByte(a uint16) byte {
+	return r[a]
+}
+
+func (r *RAM) SetByte(v byte, a uint16) byte {
+	r[a] = v
 	return r[a]
 }
